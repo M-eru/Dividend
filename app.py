2762152a46b06d76b548f373a498e1452e197f6f
Meru <mmeru.dev@gmail.com>
2022-08-25 10:38:49 +0800

    Created webapp + Added list of stocks

---
+++ app.py
@@ -0,0 +1,53 @@
+from urllib import request
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__, template_folder="templates")
+
+
+def query():
+    dict = {}
+    data = requests.get("https://www.dividends.sg/")
+    soup = BeautifulSoup(data.text, "html.parser")
+    for link in soup.find_all('a', {'target': '_blank'}):
+        title = link.get('title')
+        stats = link.get('href')
+        if title != "" and title != None:
+            dict[title] = stats
+    return dict
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/stocks")
+def stocks():
+    dict = query()
+    return render_template("stocks.html", data=dict)
+
+
+@app.route("/stocks/<name>")
+def views(name):
+    dict = query()
+    for key in dict:
+        if key == name:
+            yields = {}
+            data = requests.get(f"https://www.dividends.sg{dict[key]}")
+            # print(dict[key])
+            # print(data.text)
+            soup = BeautifulSoup(data.text, "html.parser")
+            for data in soup.find_all('td', {'class': None, 'rowspan': None, 'vertical-align': None}):
+                value = data.text.strip()
+                if 'SGD' in value or value == '-':
+                    print(f"Amount: {value}")
+                else:
+                    print(f"Ex Date: {value}")
+            break
+    return render_template("view.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
